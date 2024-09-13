3a47f210b9200b5af0f6852b0555d8cafebcb518
Pabsyyy <christiangalinato1218@gmail.com>
2024-09-13 14:00:40 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = "Christian Galinato";
+console.log(name);
+
+let whatDoYouWannaBecomeinYourLife = "programmer";
+console.log(whatDoYouWannaBecomeinYourLife);
+
+let gender = "Male";
+console.log(gender);
+
+let twitterHandle = "@pabssyyy";
+console.log(twitterHandle);
